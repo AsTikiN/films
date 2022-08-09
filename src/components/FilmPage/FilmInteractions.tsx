b5f6ffb43b9b6b4fb46f2d95998c118bebb695ec
sashka311 <ihor.garashchenko@gmail.com>
2022-08-09 10:52:55 +0300

    chore(FilmPage/FilmInterations): fixed rerender due to adding film to saved/favorite

---
+++ src/components/FilmPage/FilmInteractions.tsx
@@ -0,0 +1,71 @@
+import React, { FC, useEffect, useState } from "react";
+import PrimaryButton from "../UI/Buttons/PrimaryButton";
+import { BsBookmark, BsBookmarkFill, BsStar, BsStarFill } from "react-icons/bs";
+import { Stack } from "@mui/material";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+
+interface FilmInteractionsProps {
+  kinopoiskId: number;
+}
+
+const FilmInteractions: FC<FilmInteractionsProps> = ({ kinopoiskId }) => {
+  const [isSaved, setIsSaved] = useState(false);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const { addSavedFilm, addFavoriteFilm, removeSavedFilm, removeFavoriteFilm } = useActions();
+  const savedFilms = useTypedSelector((state) => state.film.savedFilms);
+  const favoriteFilms = useTypedSelector((state) => state.film.favoriteFilms);
+
+  const handleToggleSavedFilm = () => {
+    const filmId = kinopoiskId;
+    if (!filmId) return;
+
+    if (!savedFilms.includes(filmId)) {
+      addSavedFilm(filmId);
+    } else {
+      removeSavedFilm(filmId);
+    }
+  };
+
+  const handleToggleFavoriteFilm = () => {
+    const filmId = kinopoiskId;
+    if (!filmId) return;
+
+    if (!favoriteFilms.includes(filmId)) {
+      addFavoriteFilm(filmId);
+    } else {
+      removeFavoriteFilm(filmId);
+    }
+  };
+
+  useEffect(() => {
+    if (kinopoiskId) {
+      setIsSaved(savedFilms.includes(kinopoiskId));
+      localStorage.setItem("savedFilms", JSON.stringify(savedFilms));
+    }
+  }, [savedFilms]);
+
+  useEffect(() => {
+    if (kinopoiskId) {
+      setIsFavorite(favoriteFilms.includes(kinopoiskId));
+      localStorage.setItem("favoriteFilms", JSON.stringify(savedFilms));
+    }
+  }, [favoriteFilms]);
+
+  return (
+    <Stack direction={"row"} spacing={"10px"}>
+      <PrimaryButton size={"medium"} color={"gradient"}>
+        Смотреть
+      </PrimaryButton>
+      <PrimaryButton onClick={handleToggleSavedFilm} size={"small"}>
+        {isSaved ? <BsBookmarkFill /> : <BsBookmark />}
+      </PrimaryButton>
+      <PrimaryButton onClick={handleToggleFavoriteFilm} size={"small"}>
+        {isFavorite ? <BsStarFill /> : <BsStar />}
+      </PrimaryButton>
+    </Stack>
+  );
+};
+
+export default FilmInteractions;
---
+++ src/components/FilmPage/FilmPage.tsx
@@ -0,0 +1,91 @@
+import React, { useEffect, useState } from "react";
+import MainLayout from "../../MainLayout";
+import { useRouter } from "next/router";
+import { useGetFilmQuery } from "../../store/filmsApi/filmsApi";
+import { GetServerSideProps } from "next";
+import { Container, styled } from "@mui/system";
+import { Stack } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import { Film } from "../../types/Film";
+import PrimaryButton from "../../components/UI/Buttons/PrimaryButton";
+import { BsBookmark, BsBookmarkFill, BsStar, BsStarFill } from "react-icons/bs";
+import { useActions } from "../../hooks/useActions";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
+import Loader from "../../components/UI/Loaders/Loader";
+import FilmInteractions from "./FilmInteractions";
+
+const Wrapper = styled("div")({
+  height: "calc(100vh - 102px)",
+  width: "100%",
+  background: "url(https://i.im.ge/2022/08/06/FgYHVr.image-2022-08-06-14-42-20.png)",
+  backgroundRepeat: "no-repeat",
+  backgroundSize: "cover",
+  backgroundColor: "rgba(0,0,0,0.6)",
+  backgroundBlendMode: "multiply",
+  backgroundPosition: "center center",
+  color: "white",
+  padding: "0px 30px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+});
+
+const ContentWrapper = styled(Stack)({
+  width: "55%",
+});
+
+const Title = styled("div")({
+  fontWeight: "bold",
+  fontSize: "65px",
+  fontFamily: "Lato, sans-serif",
+});
+
+const Genres = styled("div")({
+  fontSize: "25px",
+});
+
+const Description = styled("div")({
+  fontSize: "18px",
+});
+
+const normalizeGenres = (genres: any[]) => {
+  let normalizedGenres = "";
+
+  genres.forEach((elem, index) => {
+    normalizedGenres += elem.genre[0].toUpperCase() + elem.genre.slice(1);
+
+    if (index + 1 < genres.length) {
+      normalizedGenres += " / ";
+    }
+  });
+
+  return normalizedGenres;
+};
+
+const CurrentFilm = () => {
+  const router = useRouter();
+
+  // @ts-ignore
+  const { data } = useGetFilmQuery(parseInt(router.query.id));
+
+  return (
+    <MainLayout>
+      <Wrapper>
+        {data ? (
+          <Container maxWidth={"lg"}>
+            <ContentWrapper direction="column" justifyContent="center" alignItems="flex-start" spacing={"35px"}>
+              <Title>{data.nameRu}</Title>
+              <Genres>{normalizeGenres(data.genres)}</Genres>
+              <Description>{data.description}</Description>
+              <FilmInteractions kinopoiskId={data.kinopoiskId} />
+            </ContentWrapper>
+          </Container>
+        ) : (
+          <Loader size={100} color="secondary" />
+        )}
+      </Wrapper>
+    </MainLayout>
+  );
+};
+
+export default CurrentFilm;
